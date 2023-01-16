b2763af9bf8ad8a1560995cfd7b1459a68925379
azhan3 <azhan3@ocdsb.ca>
2023-01-15 22:55:49 -0500

    testing assets

--- main.js
+++ main.js
@@ -62,7 +62,7 @@ function addStar() {
 
 Array(200).fill().forEach(addStar);
 
-const spaceTexture = new THREE.TextureLoader().load('/azhan3/assets/space.png');
+const spaceTexture = new THREE.TextureLoader().load('assets/space.png');
 scene.background = spaceTexture;
 
 
@@ -70,7 +70,7 @@ const loader = new GLTFLoader();
 
 var volleyball;
 
-loader.load( '/azhan3/assets/voleyball.glb', function ( gltf ) {
+loader.load( 'assets/voleyball.glb', function ( gltf ) {
   volleyball = gltf.scene;
 	//scene.add( volleyball);
 
@@ -82,7 +82,7 @@ loader.load( '/azhan3/assets/voleyball.glb', function ( gltf ) {
 
 
 var world;
-loader.load( '/azhan3/assets/world.glb', function ( gltf ) {
+loader.load( 'assets/world.glb', function ( gltf ) {
   world = gltf.scene;
   world.position.set(10, 25, 5);
   world.scale.set(2, 2, 2)
@@ -106,7 +106,7 @@ var windmill;
 
 
 var duck;
-loader.load( '/azhan3/assets/Duck.glb', function ( gltf ) {
+loader.load( 'assets/Duck.glb', function ( gltf ) {
   duck = gltf.scene;
   duck.position.set(100, 0, -100);
   duck.scale.set(15, 15, 15);
@@ -158,7 +158,7 @@ function moveCamera(){
   if (t < -625 && (prevT - t) > 0 && !About_me_boundary) {
     About_me_boundary = true;
     console.log("YESSS");
-    loader.load( '/azhan3/assets/windmill.glb', function ( gltf ) {
+    loader.load( 'assets/windmill.glb', function ( gltf ) {
       windmill = gltf.scene;
       windmill.name = "windmill";
       windmill.position.set(28, 49, 100);
